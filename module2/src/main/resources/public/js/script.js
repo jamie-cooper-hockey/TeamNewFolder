f9e4c2af3109655b9a278e7bfb14ddd2ef11384f
charlie <charlie.wilkening@informed.com>
2021-11-17 14:45:23 +0000

    jQuery_2_temp

--- module2/src/main/resources/public/js/script.js
+++ module2/src/main/resources/public/js/script.js
@@ -1,29 +1,15 @@
-// invoke jQuery
-$(function () {
-    // listen to the button click event
-    $('#btnGetPhotos').click(function () {
-        // empty out the 'output'
-        $('#output').empty()
-        // make a GET call to the API
-        $.get('https://jsonplaceholder.typicode.com/photos', function (photos) {
-            // construct an HTML fragment
-            let html = "<div class='photo'>"
-            // iterate over the returned data
-            $.each(photos, function (i, photo) { // i represents the index of the iterable
-                console.log(photo)
-                // add to the fragment of HTML
-                html += `<h3>${photo.title}</h3>`
-                // mini exercise - also write the image id and thumbnail URL as list items (as per p278)
-                // also try to show the ACTUAL thumnail images
-                html += `<ul>`
-                html += `<li>photo ID:${photo.id}</li>`
-                html += `<li>thumbnail:${photo.thumbnailUrl}</li>`
-                html += `</ul>`
-                html += `<img src='${photo.thumbnailUrl}' alt='${photo.title}' />`
-            }) // end of .each iterator function
-            // inject the HTML fragment into the page
-            html += '</div>'
-            $('#output').append($(html)) // inject the LITERAL object called html
-        }) // end of .get call-back function
-    }) // end of click event handler call-back
-}) // end of jQuery call-back
+$(document).ready(function() {
+    $("#show").click(function() {
+      console.log("running click on show");
+      event.preventDefault();
+      $.get("http://localhost:8080/bookshop/list", function(books) {
+            console.log(books);
+            let html = "<div class='book'>";
+            html += JSON.stringify(books);
+  
+            html += "</div>";
+            $("#booklist").append($(html));
+      });
+    });
+  });
+  
